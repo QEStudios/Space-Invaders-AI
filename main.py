08239cacdb4abb74071deb460fa22ba182d11768
SKM GEEK <38995820+QEStudios@users.noreply.github.com>
2023-09-04 10:56:50 +1000

    Commit

--- main.py
+++ main.py
@@ -144,6 +144,8 @@ try:
             lastStates.pop(0)
             lastStates.append(state)
 
+            print(f"Episode: {episode}, Current reward: {episodeReward}", end='\r')
+
             if done:
                 break
 
