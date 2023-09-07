800f343810928a58eba7b43770b108e33d377991
SKM GEEK <38995820+QEStudios@users.noreply.github.com>
2023-09-07 10:38:11 +1000

    Commit

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ for episode in range(numEpisodes):
 
             QValues[i] = target
 
-        mainModel.fit(np.expand_dims(preprocess(statesBatch), -1), QValues, epochs=50)
+        mainModel.fit(np.expand_dims(preprocess(statesBatch), -1), QValues, epochs=5)
     if episode % targetUpdateFrequency == 0:
         targetModel.set_weights(mainModel.get_weights())
 
